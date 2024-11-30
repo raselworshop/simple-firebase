edbb316d88d1923ae6a8db7a03d10d009ca9e5ea
raselworshop <raselworshop@gmail.com>
2024-11-30 23:56:01 +0800

    added env file

--- src/firebase/Firebase.init.js
+++ src/firebase/Firebase.init.js
@@ -9,13 +9,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
- 
-  authDomain: "simple-firebase-c39f2.firebaseapp.com",
-  projectId: "simple-firebase-c39f2",
-  storageBucket: "simple-firebase-c39f2.firebasestorage.app",
-  messagingSenderId: "1089214100541",
-  appId: "1:1089214100541:web:2c92c8a40071491e90a654",
-  measurementId: "G-XLESWVY333"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId
 };
 
 // Initialize Firebase
